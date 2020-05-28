00a12d16fb8dc9baca4a133c255e5de65d09f852
kanami.ogawa <gapkana@gmail.com>
2020-05-28 23:20:52 +0900

    add background color

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import * as THREE from './node_modules/three/src/Three.js';
 
 var scene = new THREE.Scene();
+
 var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshNormalMaterial( { color: 0xFFA500 } );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshDepthMaterial(0xfcf9e9);
 var cube1 = new THREE.Mesh( geometry, material );
 var cube2 = new THREE.Mesh( geometry, material );
 var cube3 = new THREE.Mesh( geometry, material );
 scene.add( cube1 );
 scene.add( cube2 );
 scene.add( cube3 );
+
+// set background color in scene
+scene.background = new THREE.Color( "rgb(137,222,184)" );
 
 camera.position.z = 5;
 var animate = function () {
@@ -29,7 +33,7 @@ var animate = function () {
   cube3.position.set( -2, 3, 1 );
   cube3.rotation.x += -0.01;
   cube3.rotation.y += -0.05;
-  cube3.translateZ( 10 );
+  //cube3.translateX( 5 );
 	renderer.render( scene, camera );
 };
 
